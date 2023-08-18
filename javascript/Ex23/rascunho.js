0306724450d513f719655772e3b90084943a76ee
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2023-08-18 11:26:17 -0300

    Finalização do exercicio da idade

---
+++ javascript/Ex23/rascunho.js
@@ -0,0 +1,7 @@
+function somar(){
+    var anoRef = document.getElementById('ano').value;
+    var nascimento = document.getElementById('nascimento').value;
+
+    anoRef = parseInt(anoRef);
+    nascimento = parseInt(nascimento);
+}
