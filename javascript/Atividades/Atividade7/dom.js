b5066dcf075d42b1392f41856ebc4f65191f756a
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2023-08-23 16:41:49 -0300

    Projeto da lista

---
+++ javascript/Atividades/Atividade7/dom.js
@@ -0,0 +1,7 @@
+let objetos = ['Cadeira', 'impresora', 'Garfo'];
+let adicionar = document.getElementById('adicionar');
+let ordenar = document.getElementById('ordenar');
+
+adicionar.addEventListener('click', function(){
+
+})
