58117102398f4a26abcb41a17fbbec526f2dbb14
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2023-09-05 18:04:49 -0300

    Update ex001.py

--- Python/Lista1/ex001.py
+++ Python/Lista1/ex001.py
@@ -1,10 +1,10 @@
 print('Controle de pescado !')
 pescados = float(input('informe a quantidade de Kilo pescado: '))
-
+multa = (pescados - 50)*4
 if pescados <= 50:
     print('Você atingiu a sua cota de pesca, não há multa !')
 else:
-    multa = pescados - 50
-    print('O valor da multa será de R$ ' + multa)
+   
+    print('O valor da multa será de R$ {:.2f}' .format(multa))
 
 
