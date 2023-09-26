e3dc8bae7880a62a709590c3ccd378fc8c52afca
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2023-09-26 11:12:43 -0300

    rascunho validador cpf

---
+++ Python/Cpf/Validacao.py
@@ -0,0 +1,3 @@
+# primeiro calculo multiplicar os primeiros numeros (9 numeros) de 10 ate 2, soma divide por 11 para dar resto % e faz 11 - resto
+# segundo é quase iqual ao primeiro vou multiplicar os primeiros numeros de 11 ate 2 (o dois sera para o digito discoberto acima), soma divide por 11 para dar resto % e faz 11 - resto
+# se o codigo acima o valor for maior que 9 converte a zero
