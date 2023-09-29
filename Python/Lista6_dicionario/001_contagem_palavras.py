04a08f98d6d8ff39b255ea7d121cc6f73ebe3b5f
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2023-09-29 16:54:05 -0300

    Update 001_contagem_palavras.py

--- Python/Lista6_dicionario/001_contagem_palavras.py
+++ Python/Lista6_dicionario/001_contagem_palavras.py
@@ -3,6 +3,10 @@
         filtre os alunos que têm uma nota superior a 85 e crie um novo dicionário
          com esses alunos.'''
 
+
+
+
+
 cadastro = {
     "nomes": [],
     "notas1": [],
@@ -11,6 +15,23 @@ cadastro = {
     "notas4": [],
 }
 
-
 while True:
-    cadastro {"nomes"} = input("Nome do aluno: ")
+
+ nomes = input("Digite o nome do aluno: ")
+ cadastro['nomes'].append(nomes)
+ notas1 = float(input("Digite a primeira nota: "))
+ cadastro['notas1'].append(notas1)
+ notas2 = float(input("Digite a segunda nota: "))
+ cadastro['notas2'].append(notas2)
+ notas3 = float(input("Digite a terceira nota: "))
+ cadastro['notas3'].append(notas3)
+ notas4 = float(input("Digite a quarta nota: "))
+ cadastro['notas4'].append(notas4)
+ pergunta = input("Deseja adicionar mais um aluno? [S/N] ").strip().upper()
+ if pergunta == 'N':    
+     break
+ 
+
+print(cadastro)
+x = zip(cadastro['nomes'], cadastro['notas1'], cadastro['notas2'], cadastro['notas3'], cadastro['notas4'])
+print(x)
