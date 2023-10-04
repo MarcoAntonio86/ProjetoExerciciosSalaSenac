6dfde4aba6d1e8f8178dc0f1f023dfad1577358a
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2023-10-04 16:29:50 -0300

    Create 2_ Dicionário_ de_ Contatos.py

---
+++ Python/projetos4_Dicionario/2_ Dicionário_ de_ Contatos.py
@@ -0,0 +1,52 @@
+'''Desenvolva um programa de agenda de contatos. Os usuários podem adicionar, 
+visualizar, editar e excluir contatos. Use um dicionário onde as chaves são os nomes 
+dos contatos e os valores são os detalhes de contato (número de telefone, email, 
+etc.).'''
+
+import os
+os.system('cls')
+
+contatos = {}
+
+menu = ('Menu: \n'
+    '1. Adicionar contato \n'
+    '2. Remover contato \n'
+    '3. Visualizar contatos \n'
+    '4. Sair \n')
+
+print(menu)
+
+escolha = 0
+
+while escolha != 4:
+    escolha = int(input('Informe a opção: '))
+    if escolha == 1:
+        nome = input('Digite o nome do contato: ')
+        telefone = input('Digite o telefone: ')
+        email = input('Digite o email: ')
+        contatos[nome] = {'telefone': telefone, 'email': email}
+    elif escolha == 2:
+        if len(contatos) == 0:
+            print('Não há contatos para remover!')
+        else:
+            print('Contatos:')
+            for nome, detalhes in contatos.items():
+                print(f"Nome: {nome}, Telefone: {detalhes['telefone']}, Email: {detalhes['email']}")
+            nome_remover = input('Informe o nome do contato que deseja remover: ')
+            if nome_remover in contatos:
+                del contatos[nome_remover]
+                print(f"O contato '{nome_remover}' foi removido com sucesso!")
+            else:
+                print(f"O contato '{nome_remover}' não existe na agenda.")
+    elif escolha == 3:
+        if len(contatos) == 0:
+            print('A agenda está vazia.')
+        else:
+            print('Contatos:')
+            for nome, detalhes in contatos.items():
+                print(f"Nome: {nome}, Telefone: {detalhes['telefone']}, Email: {detalhes['email']}")
+    elif escolha == 4:
+        print("Aplicativo encerrado.")
+        break
+    else:
+        print('Opção inválida')
