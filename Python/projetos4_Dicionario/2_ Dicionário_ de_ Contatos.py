7172b369fb4b1152cc39be9f7435273d46d2d4b5
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2023-10-04 16:35:08 -0300

    Update 2_ Dicionário_ de_ Contatos.py

--- Python/projetos4_Dicionario/2_ Dicionário_ de_ Contatos.py
+++ Python/projetos4_Dicionario/2_ Dicionário_ de_ Contatos.py
@@ -12,13 +12,40 @@ menu = ('Menu: \n'
     '1. Adicionar contato \n'
     '2. Remover contato \n'
     '3. Visualizar contatos \n'
-    '4. Sair \n')
+    '4. Editar contato \n'
+    '5. Sair \n')
 
 print(menu)
 
 escolha = 0
 
-while escolha != 4:
+def exibir_contatos():
+    if len(contatos) == 0:
+        print('A agenda está vazia.')
+    else:
+        print('Contatos:')
+        for nome, detalhes in contatos.items():
+            print(f"Nome: {nome}, Telefone: {detalhes['telefone']}, Email: {detalhes['email']}")
+        print()
+
+def editar_contato():
+    if len(contatos) == 0:
+        print('A agenda está vazia.')
+    else:
+        print('Contatos:')
+        for nome, detalhes in contatos.items():
+            print(f"Nome: {nome}, Telefone: {detalhes['telefone']}, Email: {detalhes['email']}")
+        nome_editar = input('Informe o nome do contato que deseja editar: ')
+        if nome_editar in contatos:
+            print(f'Editar contato: {nome_editar}')
+            telefone = input('Digite o novo telefone: ')
+            email = input('Digite o novo email: ')
+            contatos[nome_editar] = {'telefone': telefone, 'email': email}
+            print(f"O contato '{nome_editar}' foi atualizado com sucesso!")
+        else:
+            print(f"O contato '{nome_editar}' não existe na agenda.")
+
+while escolha != 5:
     escolha = int(input('Informe a opção: '))
     if escolha == 1:
         nome = input('Digite o nome do contato: ')
@@ -29,9 +56,7 @@ while escolha != 4:
         if len(contatos) == 0:
             print('Não há contatos para remover!')
         else:
-            print('Contatos:')
-            for nome, detalhes in contatos.items():
-                print(f"Nome: {nome}, Telefone: {detalhes['telefone']}, Email: {detalhes['email']}")
+            exibir_contatos()
             nome_remover = input('Informe o nome do contato que deseja remover: ')
             if nome_remover in contatos:
                 del contatos[nome_remover]
@@ -39,13 +64,10 @@ while escolha != 4:
             else:
                 print(f"O contato '{nome_remover}' não existe na agenda.")
     elif escolha == 3:
-        if len(contatos) == 0:
-            print('A agenda está vazia.')
-        else:
-            print('Contatos:')
-            for nome, detalhes in contatos.items():
-                print(f"Nome: {nome}, Telefone: {detalhes['telefone']}, Email: {detalhes['email']}")
+        exibir_contatos()
     elif escolha == 4:
+        editar_contato()
+    elif escolha == 5:
         print("Aplicativo encerrado.")
         break
     else:
