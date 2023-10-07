011042ef3fc97c5b65fe1cd8efa2ac9a770a3e56
MarcoAntonio86 <137942922+MarcoAntonio86@users.noreply.github.com>
2023-10-07 20:01:02 -0300

    primeiras inter_face

--- Python/inter_face/treino.py
+++ Python/inter_face/treino.py
@@ -1,5 +1,33 @@
-import  tkinter as tk
 
-janela = tk()
-janela.title("Insira aqui o seu título")
-texto = Label(janela, text="Insira aqui o seu texto botão")
+
+import requests
+from tkinter import *
+
+def pegar_cotacoes():
+    requisicao = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
+
+    requisicao_dic = requisicao.json()
+
+    cotacao_dolar = requisicao_dic['USDBRL']['bid']
+    cotacao_euro = requisicao_dic['EURBRL']['bid']
+    cotacao_btc = requisicao_dic['BTCBRL']['bid']
+
+    texto = f'''
+    Dólar: {cotacao_dolar}
+    Euro: {cotacao_euro}
+    BTC: {cotacao_btc}'''
+
+    texto_cotacoes['text'] = texto
+
+
+janela = Tk()
+janela.title('cotação atual das moedas')
+janela.geometry('500x300')
+texto_orientação = Label(janela, text='click no botão para saber as cotações das moedas')
+texto_orientação.grid(column=0, row=0, padx=10, pady=10)
+botao = Button(janela, text='Buscar cotações Dolar/Euro/BTC', command= pegar_cotacoes )
+botao.grid(column=0, row=1, padx=10, pady=10)
+texto_cotacoes = Label(janela, text='')
+texto_cotacoes.grid(column=0, row=2, padx=10, pady=10)
+
+janela.mainloop()
